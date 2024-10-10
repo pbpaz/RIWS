c47189c7c4346f1bd62391d736e508991057dc00
d-gruiz <dani.gruiz.29@gmail.com>
2024-10-10 18:52:56 +0200

    paz_spider updated with the rules for navigation

--- riws/riws/settings.py
+++ riws/riws/settings.py
@@ -17,7 +17,7 @@ NEWSPIDER_MODULE = "riws.spiders"
 #USER_AGENT = "riws (+http://www.yourdomain.com)"
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
--- riws/riws/spiders/paz_spider.py
+++ riws/riws/spiders/paz_spider.py
@@ -1,17 +1,37 @@
 import scrapy
 from riws.items import PazBookItem 
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
 
-class PazSpider(scrapy.Spider):
+class PazSpider(CrawlSpider):
     name="paz_spider"
+    
+    allowed_domains = ['www.librariapaz.gal']
 
     #Start URL
     start_urls = [
 
-        'https://www.librariapaz.gal/es/libro/asesinato-en-el-orient-express_520089'
+        'https://www.librariapaz.gal/es/index.php'
     ]
 
-    #Parse method to extract the information
+
+    #Rules for the spider
+    rules = (
+
+        #Rule for the concrete book
+        Rule(LinkExtractor(allow=(r'/es/libro/')), callback='parse', follow = False),
+        
+        #Rule for the navigation on the sections
+        Rule(LinkExtractor(allow=(r'/es/libros-de/.+pagSel=\d+')), follow=True),
+
+        #Rule for the sections
+        Rule(LinkExtractor(allow=(r'/es/libros-de/ficcion-020B/')), follow = True),
+    )
+
+
+    #Parse method to extract the information of the concrete book
     def parse(self, response):
+
         for book in response.css('div.fichaDetalle.col-sm-9'):
             item = PazBookItem()
             item['name'] = book.css('h1#titulo::text').get()
@@ -23,10 +43,4 @@ class PazSpider(scrapy.Spider):
             item['pages'] = book.css('dt:contains("Páginas") + dd::text').get()
             item['synopsis'] = ' '.join(book.xpath('//*[@id="tabsinopsis"]/p[@class="bodytext"]//text()').getall()).strip()
 
-
-
-
             yield item
-
-        #Follow the next page link if available
-
